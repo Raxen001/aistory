70dee2f78a2614bd134f8e9ab5dc6da6ee998dda
raxen001 <raxen001@tutamail.com>
2025-01-01 13:41:30 +0530

    template

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+async function getData() {
+  const url = "http://localhost:5000/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
---
+++ backend/utils.js
@@ -0,0 +1,10 @@
+/**
+ * Processes an array of numbers.
+ * @param {image[]} image - An array of images to be concatinated
+ * @param {audio} audio - audio file to add to the images
+ * @param {srt} subtitles - subtitles file to be added
+ * @return {url} - an url of the video file in the tmp
+ */
+function combine(image, audio, srt){
+    return "http://<domain>/session/UUID.mp4"
+}
