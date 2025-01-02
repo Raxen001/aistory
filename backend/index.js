0edc1e3671d156262161238e543ee3bbb81ce1df
raxen001 <raxen001@tutamail.com>
2025-01-02 20:45:41 +0530

    working on the backend man;

--- backend/index.js
+++ backend/index.js
@@ -14,3 +14,16 @@ async function getData() {
 }
 
 getData();
+
+
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`[info] ffmpeg-api listening at http://localhost:${port}`)
+});
+
