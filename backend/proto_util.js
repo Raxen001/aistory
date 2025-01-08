02b1fe88f110e8e7323919a1761f91e2c65c2b20
raxen001 <raxen001@tutamail.com>
2025-01-08 23:19:02 +0530

    backend code template;

---
+++ backend/proto_util.js
@@ -0,0 +1,14 @@
+// TODO
+// /tmp/session(UUID)/
+//                   |
+//                   |-> ffmpeg-concat-options.txt
+//                   |
+//                   |-> images{1,2,3,4}.jpg
+//                   |
+//                   |-> audio.mp3
+//                   |
+//                   |-> subtitle.srt
+//                   |
+//                   |-> OUTPUT.mp4 (after ffmpeg completion)
+//
+//
