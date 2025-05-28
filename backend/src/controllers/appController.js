2ad597b8f45f2fda937db4ca5f680f0df356fb5a
raxen001 <raxen001@tutamail.com>
2025-05-28 21:33:04 +0530

    feat: added appController

---
+++ backend/src/controllers/appController.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+function appController(req, res) {
+  // call services from here
+  return { response: "hi" };
+}
+
+export default appController;
--- backend/src/routes/v1/app.route.js
+++ backend/src/routes/v1/app.route.js
@@ -1,17 +1,25 @@
 import express from "express";
-import userRequestValid from "../../validations/userRequest.js";
 // import { clerkClient, requireAuth, getAuth } from "@clerk/express";
+import userRequestValid from "../../validations/userRequest.js";
+import appController from "../../controllers/appController.js";
 
 const router = express.Router();
-
 
 // router.route("/").get(requireAuth(), async (req, res) => {
 //
 router.route("/").get(userRequestValid, async (req, res) => {
-  res.send({
-    response: "Hello world",
-  });
+  const result = appController(req, res);
 
+  if (!result) {
+    return res.send(
+      {
+        response: "500 INTERNAL SERVER ERROR",
+      },
+      500,
+    );
+  }
+
+  res.send(result);
 });
 
 export default router;
