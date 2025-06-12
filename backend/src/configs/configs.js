5d0e785234a4cb7c8c3f20ccbf3c40df7397fef6
raxen001 <raxen001@tutamail.com>
2025-06-12 23:58:40 +0530

    feat: initial testing of image on the frontend

--- backend/src/configs/configs.js
+++ backend/src/configs/configs.js
@@ -4,6 +4,8 @@ dotenv.config()
 const config = {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     FLASH_API_KEY: process.env.FLASH_API_KEY,
+    RESPOSNE_MODELS: 'gemini-2.0-flash',
+    IMAGE_MODEL: 'gemini-2.0-flash-preview-image-generation',
 }
 
 export default config
--- backend/src/services/gemini.imageGen.impl.js
+++ backend/src/services/gemini.imageGen.impl.js
@@ -1,44 +1,30 @@
-import { GoogleGenAI } from '@google/genai'
-import dotenv from 'dotenv'
-import * as fs from 'fs'
-import path from 'node:path'
+import config from '../configs/configs.js'
+import { GoogleGenAI, Modality } from '@google/genai'
 import { v4 as uuidv4 } from 'uuid'
 import ImageGeneration from './imageGen.service.js'
-
-//workaround for .env reading , found this in stack overflow , change it if needed.
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
-
-//                  conciser text
-// {
-//     "characters": [...list of characters in the passage],
-//     "setting": "Fantasy or cyberpunk or Current",
-//     "time": "approximate time the passage takes place",
-//     "story": "sample phrase"
-// }
+import fs from 'fs'
+import path from 'path'
 
 export class ImageGenImpl extends ImageGeneration {
-    model
-    config
     ai
-    constructor(model, config) {
+
+    constructor() {
         super()
-        const apiKey = process.env.FLASH_API_KEY
-        this.model = model
-        this.config = config
+        const apiKey = config.FLASH_API_KEY
         this.ai = new GoogleGenAI({ apiKey })
     }
+
     async generateImage(conciserText) {
-        const { characters, setting, time, story } = conciserText
-        const prompt = `An illustration of ${characters.join(', ')}. The setting is ${setting}, around ${time}. The story scene: "${story}"` // need to fine tune this further , not generating accurate images.
+        const { settings, timeOfDay, location, characters, story } =
+            conciserText
+        const prompt = `An illustration of ${characters.join(', ')}. The setting is ${location}, around ${timeOfDay}. The story scene: "${story}" in the aesthetics of "${settings}"` // need to fine tune this further , not generating accurate images.
         const result = await this.ai.models.generateContent({
-            // this thing fucks up the image sometimes. and it does nt have the idea who the characters are.. so generates random people.
-            model: this.model,
+            // venkat: this thing fucks up the image sometimes. and it does nt have the idea who the characters are.. so generates random people.
+            // raxen: hallucination ?
+            model: config.IMAGE_MODEL,
             contents: prompt,
             config: {
-                responseModalities: this.config.responseModalities,
+                responseModalities: [Modality.TEXT, Modality.IMAGE],
             },
         })
         for (const part of result.candidates[0].content.parts) {
@@ -47,17 +33,28 @@ export class ImageGenImpl extends ImageGeneration {
             } else if (part.inlineData) {
                 const buffer = Buffer.from(part.inlineData.data, 'base64')
                 const tmpDir = path.join('..', 'tmp')
+
                 if (!fs.existsSync(tmpDir)) {
                     fs.mkdirSync(tmpDir, { recursive: true })
                 }
+
                 const filePath = path.join(tmpDir, `${uuidv4()}.png`)
+                console.log(filePath)
+                // TODO: need error handling for when it can't write
                 fs.writeFileSync(filePath, buffer)
-                console.log('Image saved to:', filePath)
+
+                return filePath
             }
         }
     }
 
-    getImagePath() {
-        console.log('the image path is /tmp')
+    getImagePath(conciseText) {
+        const imagePath = this.generateImage(conciseText)
+
+        if (!fs.existsSync(imagePath)) {
+            return 'error.png'
+        }
+
+        return imagePath
     }
 }
